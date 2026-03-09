df70c38c19c5519ffddca9471374a15d43f73ce7
FELIPE FERREIRA DE FRANÇA <1051392421013@fatec.corp>
2026-03-09 08:27:20 -0300

    Atualiza descrição da página sobre

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ def home():
     return "Sistema de Gerenciamento de Biblioteca"
 
 
-
 @app.route("/sobre")
 def sobre():
-    return "Projeto desenvolvido na disciplina de Integração e Entrega Contínua"
+    return "Sistema desenvolvido em Flask para estudo de CI/CD"
 
 if __name__ == "__main__":
     app.run(debug=True)
