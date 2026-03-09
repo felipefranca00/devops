e7bbb7c265f6075fbf637cef596b4c62d7451dc0
FELIPE FERREIRA DE FRANÇA <1051392421013@fatec.corp>
2026-03-09 08:24:53 -0300

    Melhora descrição da página inicial

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Sistema de Gerenciamento de Biblioteca"
+
+
+
+@app.route("/sobre")
+def sobre():
+    return "Projeto desenvolvido na disciplina de Integração e Entrega Contínua"
+
+if __name__ == "__main__":
+    app.run(debug=True)
