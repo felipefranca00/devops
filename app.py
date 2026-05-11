dcb8783093906d701d6ec3af92d86d05dd32faed
FELIPE FERREIRA DE FRANÇA <1051392421013@fatec.corp>
2026-05-11 09:33:20 -0300

    fix: corrigindo erro

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"mensagem": "API funcionando corretamente}
+    return {"mensagem": "API funcionando corretamente"}
 
 @app.route("/status")
 def status():
